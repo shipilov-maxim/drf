37d8e57dc13f07ad7d085e07d6aa844cb9f940dc
Maxim Shipilov <skam3840@gmail.com>
2024-06-08 14:22:26 +0400

    docker-compose.yml readme

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.102']
+ALLOWED_HOSTS = ['0.0.0.0',
+                 '127.0.0.1',
+                 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -95,8 +97,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
